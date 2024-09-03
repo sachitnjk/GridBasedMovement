ffccc74bc4bf7fad831fe7dcf597c5c1676b92f8
Sachit Nanajkar <50537030+sachitnjk@users.noreply.github.com>
2024-09-03 11:12:28 +0530

    Added player basic setup, sprite imports, anims

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Header("Player References")]
+    // [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Animator playerAnimator;
+}
