c5834e34afff411857e6566ff6a450bcd7ee540c
Sachit Nanajkar <50537030+sachitnjk@users.noreply.github.com>
2024-09-03 11:20:27 +0530

    Added input system package

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,4 +8,14 @@ public class PlayerMove : MonoBehaviour
     [Header("Player References")]
     // [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Animator playerAnimator;
+
+    private bool isMoving;
+
+    private void Update()
+    {
+        if (!isMoving)
+        {
+            //Register Input and do action
+        }
+    }
 }
